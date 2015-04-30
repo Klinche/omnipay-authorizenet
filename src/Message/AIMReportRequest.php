0caa174cb8647ce5436cffc85d05362b989b52a5
Daniel Brooks <daniel_brooks@dabsquared.com>
2015-04-29 17:32:10 -0700

    Base of report requests

---
+++ src/Message/AIMReportRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Omnipay\AuthorizeNet\Message;
+
+/**
+ * Authorize.Net AIM Report Request
+ */
+class AIMReportRequest extends AbstractRequest
+{
+    protected $liveEndpoint = 'https://api.authorize.net/xml/v1/request.api';
+    protected $developerEndpoint = 'https://apitest.authorize.net/xml/v1/request.api';
+
+    protected $reportAction = '';
+
+    /**
+     * @return string
+     */
+    public function getReportAction()
+    {
+        return $this->reportAction;
+    }
+
+    /**
+     * @param string $reportAction
+     */
+    public function setReportAction($reportAction)
+    {
+        $this->reportAction = $reportAction;
+    }
+
+    public function getData()
+    {
+        $data = array();
+        $data[$this->getReportAction()] = array();
+        $data[$this->getReportAction()] = $this->getMerchantAuthentication();
+
+        return $data;
+    }
+
+
+    protected function getMerchantAuthentication()
+    {
+        $data = array();
+        $data['merchantAuthentication'] = array();
+        $data['merchantAuthentication']['name'] = $this->getApiLoginId();
+        $data['merchantAuthentication']['transactionKey'] = $this->getTransactionKey();
+
+        return $data;
+
+    }
+}
